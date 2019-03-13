917eeb4d68175f454d525b3f0173d3e2a747369a
Quynh Dao <siennad97@gmail.com>
2019-03-13 12:33:24 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const http = require('http');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }));
